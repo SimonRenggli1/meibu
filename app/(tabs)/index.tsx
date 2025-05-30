0174cb95cc27190305449006338c68c641dc5d2a
robinglo <glogowskirobin@gmail.com>
2025-05-30 09:31:56 +0200

    testing out some stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,15 @@
-import { ThemedText } from '@/components/ThemedText';
+import { ThemedText } from "@/components/ThemedText";
+import { ScrollView } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-    return (
-        <ThemedText>Home</ThemedText>
-    );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView>
+          <ThemedText>Helo</ThemedText>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
