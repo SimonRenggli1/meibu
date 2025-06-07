2f1910d576f0a168ce07877eae94ef93366e9160
Robin Glogowski <glogowskirobin@gmail.com>
2025-06-07 17:05:09 +0200

    add: newer rn-safe-area-context version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,26 @@
 import "../global.css"
 import "react-native-reanimated";
-import { Stack } from "expo-router";
-import { useFonts } from "expo-font";
+import {Stack} from "expo-router";
+import {useFonts} from "expo-font";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 
 
 export default function RootLayout() {
     const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
+        SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    });
 
-  if (!loaded) {
-    return null;
-  }
-
+    if (!loaded) {
+        return null;
+    }
 
 
     return (
         <SafeAreaProvider>
-              <Stack>
-                  <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                  <Stack.Screen name="+not-found" />
-              </Stack>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                <Stack.Screen name="+not-found"/>
+            </Stack>
         </SafeAreaProvider>
-  );
+    );
 }
